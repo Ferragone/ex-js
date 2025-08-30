4a371abaf2c506464fa37a460e37ec51d11a32c5
Artem Tykhonenko <128739984+Ferragone@users.noreply.github.com>
2025-08-30 21:18:22 +0300

    all

--- 04-strings-concatenation/finish.js
+++ 04-strings-concatenation/finish.js
@@ -10,3 +10,11 @@
  *
  * 3. Выведите значение последней переменной в консоль
  */
+
+const myName2 = 'Artem'
+const mySurname2 = 'Tykhonenko'
+const myJob2 = 'Front-end Developer'
+
+const personInfo2 = `My name is ${myName2} ${mySurname2} and I am ${myJob2}` 
+
+console.log(personInfo2)
