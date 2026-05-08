42dcc5bd37bd9b3195f38a18333dd29edb2ce778
Artem Tykhonenko <128739984+Ferragone@users.noreply.github.com>
2026-05-08 16:14:52 +0300

    all

---
+++ 12-create-object/q1.js
@@ -0,0 +1,9 @@
+const info = {
+  name: 'Artem',
+  surname: 'Tykhonenko',
+  favoriteNumber: 7,
+};
+
+const person = `My name is ${info.name} ${info.surname} and my favorite number is ${info.favoriteNumber}`;
+
+console.log(person);
